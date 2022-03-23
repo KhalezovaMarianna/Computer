3fb0ec84277d57c3dddbafa2e3a675c909d0cee6
KhalezovaMarianna <m.halezova@qa-academy.by>
2022-03-23 15:48:09 +0300

    Connected log4j and created a simple exception

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,22 +12,22 @@ public class Main {
         LOGGER.debug("debug");
         LOGGER.error("error");
 
-        // User sasha = new User("Sasha");
-        //User pasha = new User("Pasha");
-        //Email email1 = new Email("text1", "test1@gmail.ru");
-        //Email email2 = new Email("text2", "test2@gmail.ru");
-        //MacBook user1 = new MacBook("user1");
-        //Asus user2 = new Asus("user2");
-        //Asus user3 = new Asus("user2");
-        //System.out.println(user2.toString());
-        //System.out.println((user2.equals(user3)));
-        //System.out.println(user2 == user3);
-        // user1.send(pasha, sasha, email1);
-        //user2.send(sasha, pasha, email2);
-        //user1.login();
-        //MacBook.checkModel();
+         User sasha = new User("Sasha");
+        User pasha = new User("Pasha");
+        Email email1 = new Email("text1", "test1@gmail.ru");
+        Email email2 = new Email("text2", "test2@gmail.ru");
+        MacBook user1 = new MacBook("user1");
+        Asus user2 = new Asus("user2");
+        Asus user3 = new Asus("user2");
+        System.out.println(user2.toString());
+        System.out.println((user2.equals(user3)));
+        System.out.println(user2 == user3);
+         user1.send(pasha, sasha, email1);
+        user2.send(sasha, pasha, email2);
+        user1.login();
+        MacBook.checkModel();
         Dell user4 = new Dell("user4");
-        //user4.resetToDefaultAllDells();
+        user4.resetToDefaultAllDells();
         try {
             user4.showMouseModel();
         } catch (Exception e) {
