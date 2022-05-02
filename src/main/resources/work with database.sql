048f1a9d9b603863f58534e6d205699d3ef67ef8
KhalezovaMarianna <m.halezova@qa-academy.by>
2022-05-02 20:20:55 +0300

    insert in database

---
+++ src/main/resources/work with database.sql
@@ -0,0 +1,16 @@
+use Service_station;
+insert TimeToWork(TimeToWork) values (1.5);
+insert Clients(Name, FirstName, TelefonNumber) values ('Masha','Khalezova', '+375293435382');
+insert Masters(Name , FirstName , TelefonNumber) values ('German','Grozny','+37529343223');
+insert Auto(StateNumber, Model) values (1234, 'audi 80');
+insert Employeers(Employeer) values ('master');
+insert Diagnosts(Name , FirstName , TelefonNumber) values ('Egor','Kiseleu','+3753333332');
+insert Admins(Name , FirstName , TelefonNumber) values ('Natalia','Shevc','+375293838492');
+insert Costs(Services, Cost) values ('Tire change',20);
+insert Sales(Service, Conditions, Sale) values ('Tire change','4 tires at the moment',12);
+insert Garage(Adress) values ('Lenina 20');
+insert Salaries(Employeer , Salary) values ('Master', 450.50);
+insert Suppliers(Supplier,Country) values ('Audi','USA');
+insert Materials(Materials , Supplier) values ('Tire','Audi');
+insert Services(Services) values ('Tire change');
+
