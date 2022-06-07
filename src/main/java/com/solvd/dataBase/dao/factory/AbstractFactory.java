9a1fab095a3e205fa522669d01208cf637f42e05
KhalezovaMarianna <m.halezova@qa-academy.by>
2022-06-07 19:29:29 +0300

    add patterns

---
+++ src/main/java/com/solvd/dataBase/dao/factory/AbstractFactory.java
@@ -0,0 +1,14 @@
+package com.solvd.dataBase.dao.factory;
+
+public class AbstractFactory {
+    public <T> T getFactory(String implementation) {
+        switch (implementation) {
+            case "mybatis":
+                return (T) new MyBatisDAOFactory();
+            case "jdbc":
+                return (T) new JdbcDAOFactory();
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/main/java/com/solvd/dataBase/dao/factory/JdbcDAOFactory.java
@@ -0,0 +1,27 @@
+package com.solvd.dataBase.dao.factory;
+
+import com.solvd.dataBase.dao.IBaseDAO;
+import com.solvd.dataBase.dao.jdbcMySQLImpl.*;
+
+public class JdbcDAOFactory {
+    public static IBaseDAO createDAO(String dao){
+        switch (dao){
+            case "auto": {
+                return new AutoDAO();
+            }
+            case "diagnost":{
+                return new DiagnostDAO();
+            }
+            case "garage":{
+                return new GarageDAO();
+            }
+            case "master":{
+                return new MastersDAO();
+            }
+            case "timeToWork":{
+                return new TimeToWorkDAO();
+            }
+        }
+        return null;
+    }
+}
